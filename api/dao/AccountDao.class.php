a82d33899682e230fa4fd8c424905262fba11ffa
ajla.sisic <ajla.sisic@stu.ibu.edu.ba>
2021-04-12 12:42:32 +0200

    add get accounts method and search option on accounts

--- api/dao/AccountDao.class.php
+++ api/dao/AccountDao.class.php
@@ -6,6 +6,12 @@ class AccountDao extends BaseDao{
   public function __construct(){
     parent::__construct("accounts");
   }
+  public function get_accounts($search, $offset, $limit){
+     return $this->query("SELECT *
+                         FROM accounts
+                         WHERE LOWER(username) LIKE CONCAT('%',:username,'%')
+                         LIMIT ${limit} OFFSET ${offset}", ["username"=>strtolower($search)]);
 
+}
 }
 ?>
--- api/index.php
+++ api/index.php
@@ -3,34 +3,17 @@
 require dirname(__FILE__).'/../vendor/autoload.php';
 require dirname(__FILE__).'/dao/AccountDao.class.php';
 
+Flight::map('query',function($name, $default_value=NULL){
+$request= Flight::request();
+$query_parameter= @$request->query->getData()[$name];
+$query_parameter= $query_parameter ? $query_parameter : $default_value;
+return $query_parameter;
+});
+/*register dao layer*/
 Flight::register('accountDao', 'AccountDao');
 
-Flight::route('GET /accounts', function(){
-  Flight::json(Flight::accountDao()->get_all(0,10));
-});
-Flight::route('GET /accounts/@id', function($id){
-    Flight::json(Flight::accountDao()->get_by_id($id));
-});
-Flight::route('POST /accounts', function(){
-/* $request=Flight::request();
-  $data=$request->data->getData();
-  print_r($request);
-  print_r($data);*/
-  $request = Flight::request();
-  $data = $request->data->getData();
-  $request = Flight::request()->data->getData();
-  Flight::json(Flight::accountDao()->add($data));
-});
-Flight::route('PUT /accounts/@id', function($id){
-  $request = Flight::request();
-$data = $request->data->getData();
-Flight::accountDao()->update($id, $data);
-$account = Flight::accountDao()->get_by_id($id);
-Flight::json($account);
-});
-Flight::route('/hello6', function(){
-    echo 'hello world6!';
-});
+/*include all routes*/
+require_once dirname(__FILE__)."/routes/accounts.php";
 
 Flight::start();
 
